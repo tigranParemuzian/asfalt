557f331916e1549a2544bb5a358f77381912501a
Tigran <tigranparemuzian@gmail.com>
2018-05-18 11:46:24 +0400

    fixes

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bin/console cache:clear && bin/console assets:install && bin/console assetic:dump && bin/console d:s:u --dump-sql && bin/console d:s:u --force
+rm -rf var/cache/ * && bin/console cache:clear && bin/console assets:install && bin/console assetic:dump && bin/console d:s:u --dump-sql && bin/console d:s:u --force
