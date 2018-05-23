1f3be954ec7714933ff613e4fdbe18b345e0b2e8
Tigran <tigranparemuzian@gmail.com>
2018-05-23 12:17:17 +0400

    fixessss choiche new verssion

--- web/app.php
+++ web/app.php
@@ -7,8 +7,8 @@ if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
-/*$kernel = new AppKernel('prod', false);
-if (PHP_VERSION_ID < 70000) {
+$kernel = new AppKernel('prod', false);
+/*if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }*/
 //$kernel = new AppCache($kernel);
